1802d4ab9b334b0732b8883e71b2c9f4848784d4
Luis Jiménez <luisjimenez6245@hotmail.com>
2022-02-23 19:19:47 -0600

    deletes authorization

--- app/app/routes/actions/__init__.py
+++ app/app/routes/actions/__init__.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
-from .authorization import router as authorization_router
 
 from . import utils
 
 router = APIRouter()
 
 router.include_router(utils.router)
-router.include_router(authorization_router, tags=["authorization"])
