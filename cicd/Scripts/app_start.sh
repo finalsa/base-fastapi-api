3bf73950ce97cab4f1ac926413cd75e785108ff4
Luis Jiménez <luisjimenez6245@hotmail.com>
2022-02-23 20:06:05 -0600

    Update app_start.sh

--- cicd/Scripts/app_start.sh
+++ cicd/Scripts/app_start.sh
@@ -2,4 +2,4 @@
 # Run App
 
 docker pull ${docker_repository}/base-api:latest
-docker-compose -f /home/ec2-user/docker-compose.yml --env-file /home/ec2-user/.env up -d
+docker-compose -f /home/ec2-user/docker-compose.prod.yml --env-file /home/ec2-user/.env up -d
